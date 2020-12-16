6fe8c23cd439c462441f9b9b471a9f2d99957880
Quentin Kermaidic <quentin.kermaidic@la-forge.co>
2020-12-16 20:40:41 +0100

    remove playground import

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import Register from './Container/Account/Register';
 import './i18n';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './Store';
-import Playground from './playground';
 
 if (!localStorage.getItem('lang')) localStorage.setItem('lang', 'fr');
 
@@ -27,7 +26,6 @@ const App: React.FC = () => (
         <Route path="/lobby/:roomId" exact component={Lobby} />
         <Route path="/game/:roomId" exact component={Game} />
         <Route path="/list" exact component={PartyList} />
-        <Route path="/playground" exact component={Playground} />
         <Route path="*" exact component={NotFound} />
       </Switch>
     </Router>
